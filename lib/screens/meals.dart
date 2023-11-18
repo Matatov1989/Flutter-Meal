ba1039ee33b71c81de6de9e8cdb24e284fb69ac9
Yury <matatov1989@gmail.com>
2023-11-18 17:56:45 +0200

    introducing thr stack widget

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -38,7 +39,7 @@ class MealsScreen extends StatelessWidget {
     if (meals.isNotEmpty) {
       content = ListView.builder(
         itemCount: meals.length,
-        itemBuilder: (ctc, index) => Text(meals[index].title),
+        itemBuilder: (ctc, index) => MealItem(meal: meals[index],),
       );
     }
 
